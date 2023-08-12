9e0bc1a1e9706ee38703f7309d520d8f49d7d40b
dumaloo <avg.dum.aloo@gmail.com>
2023-08-13 01:48:31 +0530

    Updated

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -20,9 +21,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
    print(location.longitude);
    print(location.latitude);
   }
+  
+  void getData() async {
+    var url = Uri.https(
+        'https://api.openweathermap.org/data/2.5/weather?q=London&appid=b3d752074ccc4ba43839a9ec1ca47c23',
+        '/data/2.5/weather',
+        {'q': '{http}'},
+    );
+    Response response = await get(url);
+    print(response.body);
+  }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
