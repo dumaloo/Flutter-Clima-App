b55bff390fbe845d92656fa5bb4df444c77a9497
dumaloo <avg.dum.aloo@gmail.com>
2023-08-16 00:34:57 +0530

    Updated HTTP Flutter Package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -26,8 +26,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
     var url = Uri.parse(
         'https://api.openweathermap.org/data/2.5/weather?q=London&appid=b3d752074ccc4ba43839a9ec1ca47c23',
     );
-    Response response = await get(url);
-    print(response.body);
+    http.Response response = await http.get(url);
+    print(response.statusCode);
   }
 
   @override
