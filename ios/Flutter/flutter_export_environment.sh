bd7fefba85a6077b6f4077e064f1e4fd6a546118
dumaloo <avg.dum.aloo@gmail.com>
2023-09-17 17:11:25 +0530

    dependencies changed

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/saksham/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/saksham/StudioProjects/Clima-Flutter"
+export "FLUTTER_APPLICATION_PATH=/home/saksham/StudioProjects/Flutter-Clima-App"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
