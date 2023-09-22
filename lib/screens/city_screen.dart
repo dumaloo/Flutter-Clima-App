a2bf3df2cc159fdaa20489545c7a2ea810e6ae71
dumaloo <avg.dum.aloo@gmail.com>
2023-09-22 13:17:31 +0530

    added the city screen for the user

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -7,6 +7,7 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+  String cityName = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,17 +27,28 @@ class _CityScreenState extends State<CityScreen> {
                 child: TextButton(
                   onPressed: () {},
                   child: Icon(
-                    Icons.arrow_back_ios,
+                    Icons.arrow_back_rounded,
+                    color: Colors.white,
                     size: 50.0,
                   ),
                 ),
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: kTextFieldInputDecoration,
+                  onChanged: (value) {
+                    cityName = value;
+                  },
+                ),
               ),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
                 child: Text(
                   'Get Weather',
                   style: kButtonTextStyle,
